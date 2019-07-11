b36350cb3287a59e7a4c38294e4efb955857c52d
Srijit Boinapally <srijit.rao@gmail.com>
2019-07-11 13:28:36 -0700

    Initial Commit

---
+++ Dayly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dayly
+//
+//  Created by Srijit Boinaplly on 7/11/19.
+//  Copyright © 2019 Srijit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
