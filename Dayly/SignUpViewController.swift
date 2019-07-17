1c92645a0f2033899781db894334568ed73bba63
Srijit Boinapally <srijit.rao@gmail.com>
2019-07-17 11:35:44 -0700

    trial

---
+++ Dayly/SignUpViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SignUpViewController.swift
+//  Dayly
+//
+//  Created by Srijit Boinaplly on 7/17/19.
+//  Copyright © 2019 Srijit. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignUpViewController: ViewController {
+
+   
+    @IBOutlet weak var passwordConfirm: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction override func signUpAction(_ sender: Any) {
+        if password.text != passwordConfirm.text {
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else{
+            Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "signupToHome", sender: self)
+                }
+                else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Dayly/ViewController.swift
+++ Dayly/ViewController.swift
@@ -11,36 +11,16 @@ import Firebase
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var password: UITextField!
-    @IBOutlet weak var email: UITextField!
-    @IBOutlet weak var passwordConfirm: UITextField!
+//    @IBOutlet weak var password: UITextField!
+//    @IBOutlet weak var email: UITextField!
+//    @IBOutlet weak var passwordConfirm: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func signUpAction(_ sender: Any) {
-        if password.text != passwordConfirm.text {
-            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
-            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            
-            alertController.addAction(defaultAction)
-            self.present(alertController, animated: true, completion: nil)
-        }
-        else{
-            Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
-                if error == nil {
-                    self.performSegue(withIdentifier: "signupToHome", sender: self)
-                }
-                else{
-                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                    
-                    alertController.addAction(defaultAction)
-                    self.present(alertController, animated: true, completion: nil)
-                }
-            }
-        }
+       
     }
     
     @IBAction func LoginTapped(_ sender: Any) {
