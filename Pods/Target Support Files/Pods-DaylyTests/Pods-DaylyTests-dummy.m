bd3ecb318613d32ed93db65ffb2fb477e386972d
Srijit Boinapally <srijit.rao@gmail.com>
2019-08-08 10:49:19 -0700

    c

---
+++ Pods/Target Support Files/Pods-DaylyTests/Pods-DaylyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DaylyTests : NSObject
+@end
+@implementation PodsDummy_Pods_DaylyTests
+@end
---
+++ Pods/Target Support Files/Pods-DaylyUITests/Pods-DaylyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DaylyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DaylyUITests
+@end
